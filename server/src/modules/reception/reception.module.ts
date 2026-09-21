5ed31fb16efb375ae714fc273b7f8190fdf72a48
ManhT <manhthi205@gmail.com>
2026-09-21 22:13:44 +0700

    feat(reception): collect counter payments and reprint receipts

--- server/src/modules/reception/reception.module.ts
+++ server/src/modules/reception/reception.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
+import { CounterPaymentService } from './counter-payment.service';
 import { QueueNumberService } from './queue-number.service';
 import { ReceptionController } from './reception.controller';
 import { ReceptionService } from './reception.service';
@@ -7,6 +8,6 @@ import { ReceptionService } from './reception.service';
 @Module({
   imports: [DatabaseModule],
   controllers: [ReceptionController],
-  providers: [QueueNumberService, ReceptionService],
+  providers: [CounterPaymentService, QueueNumberService, ReceptionService],
 })
 export class ReceptionModule {}
